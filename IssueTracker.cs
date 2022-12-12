49c3f984bf0ebe751717c98f1143e48117ae9082
Adam Tokarski <a.tokarski126@gmail.com>
2022-12-12 14:01:42 +0100

    initial commit

---
+++ IssueTracker.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IssueTracker
+{
+    class IssueTracker
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Hello world!");
+            _ = Console.ReadLine();
+        }
+    }
+}
