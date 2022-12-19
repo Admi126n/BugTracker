fc793768086da1d7bbe0b99677ecaaee7bea7a3d
Adam Tokarski <a.tokarski126@gmail.com>
2022-12-19 10:16:16 +0100

    modify printMenu func

--- IssueTracker.cs
+++ IssueTracker.cs
@@ -8,19 +8,59 @@ namespace IssueTracker
 {
     class IssueTracker
     {
+        static List<Field> fields = new List<Field>();
         public static void Main()
         {
             for (int i = 0; i < 3; i++)
             {
-                Field x = new Field();
-                Console.WriteLine(x.ToString());
-                _ = Console.ReadLine();
+                Menu();
             }
         }
 
-        private static void PrintMenu()
+        private static void Menu()
         {
-            Console.WriteLine("1 - show ideas\n2 - show issues\n3 - show both\n4 - add new");
+            int userInput;
+            Console.WriteLine("1 - Add new\n2 - Show by type\n3 - Show by priority\n4 - Show by status");
+
+            while (true)
+            {
+                try
+                {
+                    userInput = Int16.Parse(Console.ReadLine());
+                    if (userInput > 0 && userInput < 5)
+                    {
+                        break;
+                    } else
+                    {
+                        Console.WriteLine("Invalid input (wrong number), try again\n");
+                    }
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Invalid input (not number), try again\n");
+                }
+            }
+
+            switch (userInput)
+            {
+                case 1:
+                    Field newField = new Field();
+                    fields.Add(newField);
+                    break;
+                case 2:
+                    foreach (Field field in fields)
+                    {
+                        Console.WriteLine(field.ToString());
+                    }
+                    Console.WriteLine("");
+                    break;
+                case 3:
+                    Console.WriteLine("");
+                    break;
+                case 4:
+                    Console.WriteLine("");
+                    break;
+            }
         }
     }   
 }
