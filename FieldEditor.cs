3985790d295d1197fc0fc66db07adfcfc00e807e
Adam Tokarski <a.tokarski126@gmail.com>
2022-12-27 15:20:52 +0100

    add FieldEditor class

---
+++ FieldEditor.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IssueTracker
+{
+    static class FieldEditor
+    {
+        public static void MainFieldEditor()
+        {
+
+        }
+
+        private static void PrintEditorMenu()
+        {
+            Console.Clear();
+            Console.Write("1 - Change type" +
+                "\n2 - Change priority" +
+                "\n3 - Change status" +
+                "\n4 - Delete field" +
+                "\n5 - Exit" +
+                "Option: ");
+        }
+
+
+        private static void EditField<T>() where T : Enum
+        {
+
+        }
+
+        private static void DeleteField()
+        {
+
+        }
+    }
+}
